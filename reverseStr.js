8dc5002a701d046f07da6a2c342aa55c409fbf5e
Nanda Biradar <nandabiradar99@gmail.com>
2015-12-10 23:20:23 -0800

    Update reverseStr.js

--- reverseStr.js
+++ reverseStr.js
@@ -1,3 +1,4 @@
+//This function for reverse string with iteration 
 function reversStr(str){
     var newStr = '';
       for(var i= str.length-1; i>=0; i--){
@@ -7,3 +8,22 @@ function reversStr(str){
 }
   var str ="I love JavaScript!";
  console.log(reversStr(str));
+ 
+ //revers string using inbuilt function
+ function reversStr1(str1){
+   var newStr1 = str1.split('').reverse().join('');
+   return newStr1;
+}
+  var str1 ="I love JavaScript!";
+ console.log(reversStr1(str1));
+ 
+//with recursive function
+
+function reverse (str) {
+    if (str === "") {
+        return "";
+    } else {
+        return reverse(str.substr(1)) + str.charAt(0);
+    }
+}
+console.log(reverse("abcd"));
